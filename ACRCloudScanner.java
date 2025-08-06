8641383808e14e50ab616c37076eeb78556cc564
ahhdog168-hub <ahhdog168@gmail.com>
2025-08-07 00:20:28 +0700

    Create ACRCloudScanner.java

---
+++ ACRCloudScanner.java
@@ -0,0 +1,8 @@
+public class ACRCloudScanner {
+    public static List<int[]> scan(File audioFile) {
+        // Replace with real API integration
+        List<int[]> list = new ArrayList<>();
+        list.add(new int[]{5, 15});
+        return list;
+    }
+}
